750637ec9f3e20f72f75017d49fda84201505093
Kiffin Gish <kiffin.gish@planet.nl>
2017-06-13 14:10:55 +0200

    Mirage with government_id in show template.

--- app/routes/governments/show.js
+++ app/routes/governments/show.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
+import config from 'mijn-begroting/config/environment';
 
 export default Ember.Route.extend({
+    model(params) {
+        let url = `${config.apiHost}/${config.apiNamespace}/governments/${params.government_id}?format=json`;
+        return Ember.$.get(url);
+    },
 });
